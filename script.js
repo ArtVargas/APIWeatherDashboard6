a99273b4b6601613047a0417d7b0689415bae39a
Arturo <arturodamianvargas1996@gmail.com>
2023-09-30 15:05:44 -0500

    added images folder

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //Add queryselector for search buttion//
+
+// API KEY: 223ef6828e1b3661f819f06cb10c54a5//
+var request = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+
 var CityInput = document.querySelector(".city-input");
 var searchButton = document.querySelector(".search-btn");
 
